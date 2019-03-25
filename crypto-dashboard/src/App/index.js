82396b7ab524b421508587e160d88f5e331aa5f8
ipek <ipekcivicakan@gmail.com>
2019-03-24 20:25:25 -0700

    Add App Layout

--- crypto-dashboard/src/App/index.js
+++ crypto-dashboard/src/App/index.js
@@ -2,29 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 import WelcomeMessage from './WelcomeMessage';
 import styled, { css } from 'styled-components';
-
-const MyButton = styled.div`
-  color: green;
-  ${props => props.primary && css`
-  color: palevioletred;
-`}
-`
-
-const TomatoButton = styled(MyButton)`
-  color: tomato;
-  border-color: tomato;
-`;
-
-
+import AppLayout from './AppLayout';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <WelcomeMessage />
-        <MyButton>Hello</MyButton>
-        <MyButton primary>ipek</MyButton>
-        <TomatoButton>kaan</TomatoButton>
+        <AppLayout>
+          <WelcomeMessage />
+        </AppLayout>
       </div>
 
     );
