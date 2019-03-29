51b28e5482283895d4721c576539c65f124e2f82
ipek <ipekcivicakan@gmail.com>
2019-03-28 20:06:15 -0700

    Add CoinTile

--- crypto-dashboard/src/Shared/Tile.js
+++ crypto-dashboard/src/Shared/Tile.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { subtleBoxShadow, lightBlueBackground, greenBoxShadow, redBoxShadow } from "../Shared/Styles";
+import { subtleBoxShadow, lightBlueBackground, greenBoxShadow, redBoxShadow } from "./Styles";
 
 export const Tile = styled.div`
     ${subtleBoxShadow}
