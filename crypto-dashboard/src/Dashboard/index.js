0f54528d83b1be61b371077a9444c1d3fba41131
ipek <ipekcivicakan@gmail.com>
2019-03-31 22:47:42 -0700

    Add HighCharts

--- crypto-dashboard/src/Dashboard/index.js
+++ crypto-dashboard/src/Dashboard/index.js
@@ -3,6 +3,7 @@ import Page from '../Shared/Page';
 import PriceGrid from './PriceGrid';
 import CoinSpotlight from './CoinSpotlight';
 import styled from 'styled-components';
+import PriceChart from './PriceChart';
 
 
 const ChartGrid = styled.div`
@@ -18,7 +19,7 @@ export default function () {
             <PriceGrid />
             <ChartGrid>
                 <CoinSpotlight />
-                <div>Chart goes here</div>
+                <PriceChart />
             </ChartGrid>
         </Page>
     )
