6ab74532a138dd383084b0509feb9f0c4989e640
ipek <ipekcivicakan@gmail.com>
2019-03-31 22:30:49 -0700

    Add Coin Spotlight

--- crypto-dashboard/src/App/AppProvider.js
+++ crypto-dashboard/src/App/AppProvider.js
@@ -32,7 +32,7 @@ export class AppProvider extends React.Component {
 
     fetchCoins = async () => {
         let coinList = (await cc.coinList()).Data;
-        //console.log(coinList)
+        // console.log(coinList)
         this.setState({ coinList })
         //console.log(this.state);
     }
